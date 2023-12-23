7581f943d293b0f06850e81e98250a448572cc03
leslie-zi-pan <zifeng1996@outlook.com>
2023-12-23 13:44:24 +0000

    Update to Monai 1.3.0 >=

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,12 +1,9 @@
-
-from monai.transforms import (
-    MapTransform
-)
+from monai.transforms import MapTransform
 from monai.transforms.utils import resize_center
-import numpy as np 
+import numpy as np
 import torch
 from monai.transforms import (
-    AddChanneld,
+    EnsureChannelFirstd,
     Compose,
     LoadImaged,
     MapTransform,
@@ -18,35 +15,38 @@ from monai.transforms import (
     RandScaleIntensityd,
     RandShiftIntensityd,
     Rand2DElasticd,
-    RandZoomd
+    RandZoomd,
 )
 
 from src.enums import DataDict
 from src.utils import normalize_img_intensity_range
+
 
 class ImagesToMultiChannel(MapTransform):
     def __call__(self, data):
         to_concat_list = []
         for key in self.keys:
             to_concat_list.append(data[key])
-            
+
         data[DataDict.Image] = torch.cat(to_concat_list, dim=0)
         return data
+
 
 class Unsqueeze(MapTransform):
     def __call__(self, data):
         for key in self.keys:
             data[key] = torch.unsqueeze(data[key], dim=0)
-            
+
         return data
-        
+
 
 class TestShape(MapTransform):
     def __call__(self, data):
         for key in self.keys:
-            print(f'{key}: \t{data[key].shape}')
-            
+            print(f"{key}: \t{data[key].shape}")
+
         return data
+
 
 class ResizeCentre(MapTransform):
     def __init__(self, keys, dims):
@@ -54,22 +54,23 @@ class ResizeCentre(MapTransform):
         self.dims = dims
 
     def __call__(self, data):
-        print(f'dimensions are {self.dims}')
+        print(f"dimensions are {self.dims}")
         for key in self.keys:
-            print(f'before {data[key].shape}')
+            print(f"before {data[key].shape}")
             data[key] = resize_center(data[key], *self.dims)
-            print(f'after {data[key].shape}')
-            
+            print(f"after {data[key].shape}")
+
         return data
-        
+
 
 # normalize_img_intensity_range
 class NormalizeIntensitydCustom(MapTransform):
     def __call__(self, data):
         for key in self.keys:
             data[key] = normalize_img_intensity_range(data[key])
-            
+
         return data
+
 
 class ConvertToMultiChannelBasedOnLabelsClassesd(MapTransform):
     """
@@ -80,10 +81,10 @@ class ConvertToMultiChannelBasedOnLabelsClassesd(MapTransform):
 
     def __call__(self, data):
         d = dict(data)
-        
+
         for key in self.keys:
             result = []
-            
+
             d[key] = np.squeeze(d[key])
 
             # Other labels and background merge as backgrounds
@@ -92,67 +93,100 @@ class ConvertToMultiChannelBasedOnLabelsClassesd(MapTransform):
             result.append(d[key] == 1)
 
             d[key] = np.stack(result, axis=0).astype(np.float32)
-            
+
         return d
-    
+
 
 train_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        AddChanneld(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
             pixdim=(1.5, 1.5, 2.0),
             mode=("bilinear", "bilinear", "nearest"),
         ),
-        Resized(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], spatial_size=[256, 256]), 
-        Orientationd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"),
-        RandFlipd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], prob=0.5, spatial_axis=0),
+        Resized(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            spatial_size=[256, 256],
+        ),
+        Orientationd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"
+        ),
+        RandFlipd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            prob=0.5,
+            spatial_axis=0,
+        ),
         # TestShape(keys=[DataDict.ImageT1, DataDict.ImageFlair]),
         # NormalizeIntensitydCustom(keys=[DataDict.ImageT1, DataDict.ImageFlair]),
         # NormalizeIntensityd(keys=[DataDict.ImageT1, DataDict.ImageFlair], nonzero=True, channel_wise=True),
-        Rand2DElasticd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], spacing=(30, 40), magnitude_range=(0.8, 1.2), prob=0.3),
-        RandScaleIntensityd(keys=[DataDict.ImageT1, DataDict.ImageFlair], factors=0.1, prob=0.5),
-        RandShiftIntensityd(keys=[DataDict.ImageT1, DataDict.ImageFlair], offsets=0.1, prob=0.5),
-        RandZoomd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], prob=1.0, min_zoom=0.9, max_zoom=1.1),
+        Rand2DElasticd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            spacing=(30, 40),
+            magnitude_range=(0.8, 1.2),
+            prob=0.3,
+        ),
+        RandScaleIntensityd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair], factors=0.1, prob=0.5
+        ),
+        RandShiftIntensityd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair], offsets=0.1, prob=0.5
+        ),
+        RandZoomd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            prob=1.0,
+            min_zoom=0.9,
+            max_zoom=1.1,
+        ),
         ToTensord(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]), 
+        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]),
     ]
 )
 
 val_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        AddChanneld(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
             pixdim=(1.5, 1.5, 2.0),
             mode=("bilinear", "bilinear", "nearest"),
         ),
-        Resized(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], spatial_size=[256, 256]), 
-        Orientationd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"),
+        Resized(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            spatial_size=[256, 256],
+        ),
+        Orientationd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"
+        ),
         # NormalizeIntensityd(keys=[DataDict.ImageT1, DataDict.ImageFlair], nonzero=True, channel_wise=True),
         ToTensord(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]), 
+        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]),
     ]
 )
 
 test_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        AddChanneld(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
             pixdim=(1.5, 1.5, 2.0),
             mode=("bilinear", "bilinear", "nearest"),
         ),
-        Resized(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], spatial_size=[256, 256]), 
-        Orientationd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"),
+        Resized(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
+            spatial_size=[256, 256],
+        ),
+        Orientationd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label], axcodes="RAS"
+        ),
         # NormalizeIntensityd(keys=[DataDict.ImageT1, DataDict.ImageFlair], nonzero=True, channel_wise=True),
         ToTensord(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]), 
+        ImagesToMultiChannel(keys=[DataDict.ImageT1, DataDict.ImageFlair]),
     ]
 )
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,34 +11,49 @@ class UNet(UNet):
         x = super().forward(x)
         x = F.softmax(x)
         return x
-    
+
+
 from src.utils import numpy_from_tensor
+
 ROOT_DIR = "models/"
 
-def train_network(training_loader, val_loader, network, loss_fun, optimizer, device, EPOCHS=200, pre_load_training=False, checkpoint_name=''):
+
+def train_network(
+    training_loader,
+    val_loader,
+    network,
+    loss_fun,
+    optimizer,
+    device,
+    EPOCHS=200,
+    pre_load_training=False,
+    checkpoint_name="",
+):
     # network.cuda(device)
- 
+
     optimizer = optimizer
-    loss_fun = loss_fun 
+    loss_fun = loss_fun
 
     epoch_checkpoint = 0
 
     losses = {}
     val_losses = {}
-    
+
     # Test Learning rate dictionary for visualization
     scheduler_learning_rate_dict = {}
 
     if pre_load_training:
-        checkpoint = torch.load(ROOT_DIR + f'{checkpoint_name}.pt',  map_location=torch.device('cpu'))
-        epoch_checkpoint = checkpoint['epoch'] + 1
-        network.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        loss = checkpoint['loss']
-        losses = checkpoint['losses']
-        val_losses = checkpoint['val_losses']
+        checkpoint = torch.load(
+            ROOT_DIR + f"{checkpoint_name}.pt", map_location=torch.device("cpu")
+        )
+        epoch_checkpoint = checkpoint["epoch"] + 1
+        network.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        loss = checkpoint["loss"]
+        losses = checkpoint["losses"]
+        val_losses = checkpoint["val_losses"]
         # learning_rate = checkpoint['learning_rate'] if 'learning_rate' in checkpoint.keys() else optimizer.param_groups[0]["lr"]
-    
+
     # if epoch_checkpoint > 300:
     #     print(f'Learning rate changed from {optimizer.param_groups[0]["lr"]} to:')
     #     optimizer.param_groups[0]["lr"] = 0.0001
@@ -48,29 +63,31 @@ def train_network(training_loader, val_loader, network, loss_fun, optimizer, dev
     for epoch in range(epoch_checkpoint, EPOCHS):
         network.train(True)
 
-        print(f'losses: {losses}')
-        print(f'val losses {val_losses}')
-        
+        print(f"losses: {losses}")
+        print(f"val losses {val_losses}")
+
         train_step = 1
         batch_loss = []
 
         for batch_data in training_loader:
-            print(f'Epoch {epoch}\tTraining Step: {train_step}/{len(training_loader)}')
+            print(f"Epoch {epoch}\tTraining Step: {train_step}/{len(training_loader)}")
 
             # torch.cuda.empty_cache() # Clear any unused variables
             inputs = batch_data["image"].to(device)
-            labels = batch_data["label"] # Only pass to CUDA when required - preserve memory
-            
+            labels = batch_data[
+                "label"
+            ]  # Only pass to CUDA when required - preserve memory
+
             # Zero the parameter gradients
             optimizer.zero_grad()
 
             # Feed input data into the network to train
             outputs = network(inputs)
-            
+
             # Input no longer in use for current iteration - clear from CUDA memory
             inputs = inputs.cpu()
             # torch.cuda.empty_cache()
-            
+
             # labels to CUDA
             labels = batch_data["label"].to(device)
             # torch.cuda.empty_cache()
@@ -80,14 +97,14 @@ def train_network(training_loader, val_loader, network, loss_fun, optimizer, dev
 
             # List of losses for current batch
             batch_loss.append(loss.detach().cpu().numpy())
-            
+
             # Clear CUDA memory
             labels = labels.cpu()
             # torch.cuda.empty_cache()
-            
+
             # Backward pass
             loss.backward()
-            
+
             # Optimize
             optimizer.step()
 
@@ -95,12 +112,12 @@ def train_network(training_loader, val_loader, network, loss_fun, optimizer, dev
 
         # Get average loss for current batch
         losses[epoch] = np.mean(batch_loss)
-        print(f'train losses {batch_loss} \nmean loss {losses[epoch]}')
-        
+        print(f"train losses {batch_loss} \nmean loss {losses[epoch]}")
+
         if epoch % 2 == 0:
             # Set network to eval mode
             network.train(False)
-            # Disiable gradient calculation and optimise memory 
+            # Disiable gradient calculation and optimise memory
             with torch.no_grad():
                 # Initialise validation loss
                 dice_test_loss = 0
@@ -123,47 +140,72 @@ def train_network(training_loader, val_loader, network, loss_fun, optimizer, dev
                     dice_test_loss += test_loss
                     # dice_test_loss += loss_fun(outputs, labels)
                     val_iter_count += 1
-                    print(f'Val loss iter {i}: {test_loss}')
+                    print(f"Val loss iter {i}: {test_loss}")
 
                 # Get average validation DICE CE loss
                 val_losses[epoch] = dice_test_loss / val_iter_count
-              
-                # Print errors 
+
+                # Print errors
                 print(
-                    "==== Epoch: " + str(epoch) + 
-                    " | DICE loss: " + str(numpy_from_tensor((dice_test_loss) / val_iter_count)) +
-                    " | Total Loss: " + str(numpy_from_tensor((dice_test_loss) / val_iter_count))+ " =====") # This is redundant code but will keep here incase we add more losses
-                
+                    "==== Epoch: "
+                    + str(epoch)
+                    + " | DICE loss: "
+                    + str(numpy_from_tensor((dice_test_loss) / val_iter_count))
+                    + " | Total Loss: "
+                    + str(numpy_from_tensor((dice_test_loss) / val_iter_count))
+                    + " ====="
+                )  # This is redundant code but will keep here incase we add more losses
+
                 # View slice at halfway point
                 half = outputs.shape[2] // 2
-                
-                # Show predictions for current iteration
-                print(f'shape is {inputs.shape}')
-                view_slice(numpy_from_tensor(inputs[0, 0, :, :]), f'Input Channel 0 Image Epoch {epoch}', gray=True)
-                view_slice(numpy_from_tensor(inputs[0, 1, :, :]), f'Input Channel 1 Image Epoch {epoch}', gray=True)
-                view_slice(numpy_from_tensor(outputs[0, 1, :, :]), f'WMH Output Image Epoch {epoch}', gray=True)
-                view_slice(numpy_from_tensor(labels[0, 1, :, :]), f'WMH Labels  Epoch {epoch}', gray=True)
-        
-        # Save training checkpoint
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': network.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
-            'losses': losses,
-            'val_losses': val_losses,
-            # 'learning_rate': optimizer.param_groups[0]['lr']
-            # 'scheduler_learning_rate_dict':scheduler_learning_rate_dict
-        }, ROOT_DIR + f'{checkpoint_name}.pt')
 
-        # Confirm current epoch trained params are saved 
-        print(f'Saved for epoch {epoch}')
+                # Show predictions for current iteration
+                print(f"shape is {inputs.shape}")
+                view_slice(
+                    numpy_from_tensor(inputs[0, 0, :, :]),
+                    f"Input Channel 0 Image Epoch {epoch}",
+                    gray=True,
+                )
+                view_slice(
+                    numpy_from_tensor(inputs[0, 1, :, :]),
+                    f"Input Channel 1 Image Epoch {epoch}",
+                    gray=True,
+                )
+                view_slice(
+                    numpy_from_tensor(outputs[0, 1, :, :]),
+                    f"WMH Output Image Epoch {epoch}",
+                    gray=True,
+                )
+                view_slice(
+                    numpy_from_tensor(labels[0, 1, :, :]),
+                    f"WMH Labels  Epoch {epoch}",
+                    gray=True,
+                )
+
+        # Save training checkpoint
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": network.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": loss,
+                "losses": losses,
+                "val_losses": val_losses,
+                # 'learning_rate': optimizer.param_groups[0]['lr']
+                # 'scheduler_learning_rate_dict':scheduler_learning_rate_dict
+            },
+            ROOT_DIR + f"{checkpoint_name}.pt",
+        )
+
+        # Confirm current epoch trained params are saved
+        print(f"Saved for epoch {epoch}")
 
     return network
 
+
 device = torch.device("cpu")
 model = UNet(
-    dimensions=2,
+    spatial_dims=2,
     in_channels=2,
     out_channels=2,
     channels=(16, 32, 64, 128, 256),
@@ -176,7 +218,10 @@ model = UNet(
 loss_function = DiceLoss()
 
 optimizer = torch.optim.Adam(
-    model.parameters(), 1e-3, weight_decay=1e-5, amsgrad=True, 
+    model.parameters(),
+    1e-3,
+    weight_decay=1e-5,
+    amsgrad=True,
 )
 
 # from torchvision import models
@@ -184,5 +229,5 @@ optimizer = torch.optim.Adam(
 
 # summary(model, (2, 256, 256))
 
-# net = train_network(training_loader=train_loader, val_loader=validation_loader, network=model, 
+# net = train_network(training_loader=train_loader, val_loader=validation_loader, network=model,
 #                     loss_fun=loss_function, optimizer=optimizer, device=device, EPOCHS=2000, pre_load_training=True, checkpoint_name='unet_brain_wmi_UbSi_new_norm')

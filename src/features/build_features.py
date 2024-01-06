5d9abdda7333a1e2447079332eef7f25d4e785bb
leslie-zi-pan <zifeng1996@outlook.com>
2024-01-06 12:01:30 +0000

    Separate out transforms and compositions

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,12 +1,7 @@
-from monai.transforms import MapTransform
-from monai.transforms.utils import resize_center
-import numpy as np
-import torch
 from monai.transforms import (
     EnsureChannelFirstd,
     Compose,
     LoadImaged,
-    MapTransform,
     Resized,
     Spacingd,
     ToTensord,
@@ -19,87 +14,18 @@ from monai.transforms import (
 )
 
 from src.enums import DataDict
-from src.pytorch_utils import normalize_img_intensity_range
-
-
-class ImagesToMultiChannel(MapTransform):
-    def __call__(self, data):
-        to_concat_list = []
-        for key in self.keys:
-            to_concat_list.append(data[key])
-
-        data[DataDict.Image] = torch.cat(to_concat_list, dim=0)
-        return data
-
-
-class Unsqueeze(MapTransform):
-    def __call__(self, data):
-        for key in self.keys:
-            data[key] = torch.unsqueeze(data[key], dim=0)
-
-        return data
-
-
-class TestShape(MapTransform):
-    def __call__(self, data):
-        for key in self.keys:
-            print(f"{key}: \t{data[key].shape}")
-
-        return data
-
-
-class ResizeCentre(MapTransform):
-    def __init__(self, keys, dims):
-        super().__init__(keys=keys)
-        self.dims = dims
-
-    def __call__(self, data):
-        print(f"dimensions are {self.dims}")
-        for key in self.keys:
-            print(f"before {data[key].shape}")
-            data[key] = resize_center(data[key], *self.dims)
-            print(f"after {data[key].shape}")
-
-        return data
-
-
-# normalize_img_intensity_range
-class NormalizeIntensitydCustom(MapTransform):
-    def __call__(self, data):
-        for key in self.keys:
-            data[key] = normalize_img_intensity_range(data[key])
-
-        return data
-
-
-class ConvertToMultiChannelBasedOnLabelsClassesd(MapTransform):
-    """
-    Convert labels to multi channels based on brats classes:
-    label 1 is the WMH
-
-    """
-
-    def __call__(self, data):
-        d = dict(data)
-
-        for key in self.keys:
-            result = []
-
-            d[key] = np.squeeze(d[key])
-
-            result.append(np.logical_or(d[key] == 0, d[key] == 2))
-            # Other labels and background merge as backgrounds
-            result.append(d[key] == 1)
-
-            d[key] = np.stack(result, axis=0).astype(np.float32)
-
-        return d
+from src.features.transforms import (
+    ConvertToMultiChannelBasedOnLabelsClassesd,
+    ImagesToMultiChannel,
+)
 
 
 train_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]
+        ),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
@@ -144,7 +70,9 @@ train_transform = Compose(
 val_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]
+        ),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
@@ -166,7 +94,9 @@ val_transform = Compose(
 test_transform = Compose(
     [
         LoadImaged(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
-        EnsureChannelFirstd(keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]),
+        EnsureChannelFirstd(
+            keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label]
+        ),
         ConvertToMultiChannelBasedOnLabelsClassesd(keys=[DataDict.Label]),
         Spacingd(
             keys=[DataDict.ImageT1, DataDict.ImageFlair, DataDict.Label],
---
+++ src/features/transforms.py
@@ -0,0 +1,81 @@
+from monai.transforms import MapTransform
+from monai.transforms.utils import resize_center
+import numpy as np
+import torch
+
+from src.enums import DataDict
+from src.pytorch_utils import normalize_img_intensity_range
+
+
+class ImagesToMultiChannel(MapTransform):
+    def __call__(self, data):
+        to_concat_list = []
+        for key in self.keys:
+            to_concat_list.append(data[key])
+
+        data[DataDict.Image] = torch.cat(to_concat_list, dim=0)
+        return data
+
+
+class Unsqueeze(MapTransform):
+    def __call__(self, data):
+        for key in self.keys:
+            data[key] = torch.unsqueeze(data[key], dim=0)
+
+        return data
+
+
+class TestShape(MapTransform):
+    def __call__(self, data):
+        for key in self.keys:
+            print(f"{key}: \t{data[key].shape}")
+
+        return data
+
+
+class ResizeCentre(MapTransform):
+    def __init__(self, keys, dims):
+        super().__init__(keys=keys)
+        self.dims = dims
+
+    def __call__(self, data):
+        print(f"dimensions are {self.dims}")
+        for key in self.keys:
+            print(f"before {data[key].shape}")
+            data[key] = resize_center(data[key], *self.dims)
+            print(f"after {data[key].shape}")
+
+        return data
+
+
+# normalize_img_intensity_range
+class NormalizeIntensitydCustom(MapTransform):
+    def __call__(self, data):
+        for key in self.keys:
+            data[key] = normalize_img_intensity_range(data[key])
+
+        return data
+
+
+class ConvertToMultiChannelBasedOnLabelsClassesd(MapTransform):
+    """
+    Convert labels to multi channels based on brats classes:
+    label 1 is the WMH
+
+    """
+
+    def __call__(self, data):
+        d = dict(data)
+
+        for key in self.keys:
+            result = []
+
+            d[key] = np.squeeze(d[key])
+
+            result.append(np.logical_or(d[key] == 0, d[key] == 2))
+            # Other labels and background merge as backgrounds
+            result.append(d[key] == 1)
+
+            d[key] = np.stack(result, axis=0).astype(np.float32)
+
+        return d
